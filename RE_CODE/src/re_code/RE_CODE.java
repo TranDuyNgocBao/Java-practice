2afa1d61de2393481d1648e0b7d2e08bcef9cccb
BaToRan <89400664+TranDuyNgocBao@users.noreply.github.com>
2023-12-23 20:53:11 +0700

    Add files via upload

---
+++ RE_CODE/src/re_code/RE_CODE.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package re_code;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class RE_CODE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
